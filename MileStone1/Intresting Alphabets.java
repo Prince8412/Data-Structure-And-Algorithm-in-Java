4157e488332521dec3db0f269c9f128b54dea8d0
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-09-29 17:57:45 +0000

    Intresting Alphabets

---
+++ MileStone1/Intresting Alphabets.java
@@ -0,0 +1,57 @@
+// Code : Interesting Alphabets
+// Send Feedback
+// Print the following pattern for the given number of rows.
+// Pattern for N = 5
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+// Input format :
+// N (Total no. of rows)
+// Output format :
+// Pattern in N lines
+// Constraints
+// 0 <= N <= 26
+// Sample Input 1:
+// 8
+// Sample Output 1:
+// H
+// GH
+// FGH
+// EFGH
+// DEFGH
+// CDEFGH
+// BCDEFGH
+// ABCDEFGH
+// Sample Input 2:
+// 7
+// Sample Output 2:
+// G
+// FG
+// EFG
+// DEFG
+// CDEFG
+// BCDEFG
+// ABCDEFG
+
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		//Your code goes here
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		while (i<=n) {
+			int j = 1;
+			char counter = (char)('A'+n -i);
+			while (j<=i) {
+				System.out.print(counter);
+				counter++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
