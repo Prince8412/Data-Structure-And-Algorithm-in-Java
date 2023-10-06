0b0da00160ca00d1f243ca878cd89fe2e089129f
prince raj <100460531+Prince8412@users.noreply.github.com>
2023-10-06 15:15:18 +0000

    Binary to Decimals

---
+++ MileStone1/Operators/Binary to Decimal.java
@@ -0,0 +1,36 @@
+// Binary to decimal
+// Send Feedback
+// Given a binary number as an integer N, convert it into decimal and print.
+// Input format :
+// An integer N in the Binary Format
+// Output format :
+// Corresponding Decimal number (as integer)
+// Constraints :
+// 0 <= N <= 10^9
+// Sample Input 1 :
+// 1100
+// Sample Output 1 :
+// 12
+// Sample Input 2 :
+// 111
+// Sample Output 2 :
+// 7
+
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1,rem,sum=0;
+		while(n>0){
+			rem=n%10;
+			sum=sum+(rem*i);
+			n=n/10;
+			i=i*2;
+		}
+		System.out.println(sum);
+	}
+}
